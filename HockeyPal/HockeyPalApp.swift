b928c4c5cb945976082d201ffc8ef3c9e5cbc36d
Pål <palforsberg@gmail.com>
2021-01-08 09:31:14 +0100

    Initial Commit

---
+++ HockeyPal/HockeyPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyPalApp.swift
+//  HockeyPal
+//
+//  Created by Pål Forsberg on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct HockeyPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
